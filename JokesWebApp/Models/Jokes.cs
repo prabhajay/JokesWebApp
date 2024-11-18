ed42f49ce50f31faa6c3efeb92887ef455b32e1b
Lakshmiprabha <lakshmi.prbh@gmail.com>
2024-11-17 19:40:49 -0700

    Add project files.

---
+++ JokesWebApp/Models/Jokes.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestions {  get; set; }
+        public string Answer {  get; set; }
+        public Jokes()
+        {
+
+        }
+    }
+}
